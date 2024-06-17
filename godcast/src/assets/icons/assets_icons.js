fdfbb4e1203aea8e34f192788b201136ce29acad
jv1ma <jvivesma@irsl.edu.mx>
2024-06-16 20:29:47 -0600

    Add files via upload

--- godcast/src/assets/icons/assets_icons.js
+++ godcast/src/assets/icons/assets_icons.js
@@ -2,6 +2,7 @@ import svg_whatsapp from './svg_whatsapp.svg'
 import svg_spotify from './svg_spotify.svg'
 import svg_youtube from './svg_youtube.svg'
 import svg_apple from './svg_apple.svg'
+import svg_instagram from './svg_instagram.svg'
 import svg_whatsapp_clean from './svg_whatsapp_clean.svg'
 import svg_spotify_clean from './svg_spotify_clean.svg'
 import svg_youtube_clean from './svg_youtube_clean.svg'
@@ -10,10 +11,11 @@ import svg_whatsapp_color from './svg_whatsapp_color.svg'
 import svg_spotify_color from './svg_spotify_color.svg'
 import svg_youtube_color from './svg_youtube_color.svg'
 import svg_apple_color from './svg_apple_color.svg'
+import svg_instagram_color from './svg_instagram_color.svg'
 import svg_hamburguer from './svg_hamburguer.svg'
 import svg_donate from './svg_donate.svg'
 import svg_gk from './svg_gk.svg'
 
-export { svg_whatsapp, svg_spotify, svg_youtube, svg_apple, svg_hamburguer, svg_donate, svg_whatsapp_clean, svg_apple_clean,svg_spotify_clean, svg_youtube_clean, svg_apple_color, svg_spotify_color, svg_whatsapp_color, svg_youtube_color, svg_gk }
+export { svg_instagram, svg_instagram_color, svg_whatsapp, svg_spotify, svg_youtube, svg_apple, svg_hamburguer, svg_donate, svg_whatsapp_clean, svg_apple_clean, svg_spotify_clean, svg_youtube_clean, svg_apple_color, svg_spotify_color, svg_whatsapp_color, svg_youtube_color, svg_gk }
 
 
